08d226b8482fe2fef754db2cb50378b96e2c90e6
Danilo de Souza <danilo@hanzo.com.br>
2021-10-23 14:12:31 -0300

    Funcion for loading

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ let productsPromisse = fetch(
 );
 
 productsPromisse.then((resp) => {
-  resp.json().then((product) => {
-    let products = loadProducts(product);
-    document.getElementById("catalog").innerHTML = products;
+  resp.json().then((products) => {
+    let productsHtml = loadProducts(products);
+    let brandsHtml = loadBrands(products);
+    document.getElementById("catalog").innerHTML = productsHtml;
+    document.getElementById("filter-brand").innerHTML = brandsHtml;
   });
 });
 
@@ -18,17 +20,37 @@ function loadProducts(products) {
   return rows.join("");
 }
 
+function loadBrands(products) {
+  let rows = products.map(product => product.brand)
+    .filter((value, index, self) => self.indexOf(value) == index);
+
+  let options = rows.map(brand => {
+    return `<option>${brand}</option>`
+  })
+
+  // console.log("Rows", rows[0])
+  // console.log("Rows", rows[1])
+  // let = options = `<option>${rows.join("")}</option>`
+
+  //  return `<option>${rows.join(" ")}</option>`
+  console.log("PRINT", options)
+
+  // return options;
+  return options;
+
+}
+
 //EXEMPLO DO CÓDIGO PARA UM PRODUTO
 function productItem(product) {
   const item = `<div class="product" data-name="${product.name}" data-brand="nyx" data-type="bronzer" tabindex="508">
-  <figure class="product-figure">
-    <img src="${product.image_link}">
+      <figure class="product-figure">
+        <img src="${product.image_link}">
   </figure>
-  <section class="product-description">
-    <h1 class="product-name">${product.name}</h1>
-    <div class="product-brands"><span class="product-brand background-brand">Nyx</span>
-<span class="product-brand background-price">R$ 57.70</span></div>
-  </section>
+        <section class="product-description">
+          <h1 class="product-name">${product.name}</h1>
+          <div class="product-brands"><span class="product-brand background-brand">Nyx</span>
+            <span class="product-brand background-price">R$ 57.70</span></div>
+        </section>
   // CARREGAR OS DETALHES
 </div>`;
   return item;
@@ -37,31 +59,31 @@ function productItem(product) {
 //EXEMPLO DO CÓDIGO PARA OS DETALHES DE UM PRODUTO
 function loadDetails(product) {
   let details = `<section class="product-details"><div class="details-row">
-        <div>Brand</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">nyx</div>
-        </div>
-      </div><div class="details-row">
-        <div>Price</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">10.49</div>
-        </div>
-      </div><div class="details-row">
-        <div>Rating</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">5</div>
-        </div>
-      </div><div class="details-row">
-        <div>Category</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250"></div>
-        </div>
-      </div><div class="details-row">
-        <div>Product_type</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">bronzer</div>
-        </div>
-      </div></section>`;
+          <div>Brand</div>
+          <div class="details-bar">
+            <div class="details-bar-bg" style="width= 250">nyx</div>
+          </div>
+        </div><div class="details-row">
+          <div>Price</div>
+          <div class="details-bar">
+            <div class="details-bar-bg" style="width= 250">10.49</div>
+          </div>
+        </div><div class="details-row">
+          <div>Rating</div>
+          <div class="details-bar">
+            <div class="details-bar-bg" style="width= 250">5</div>
+          </div>
+        </div><div class="details-row">
+          <div>Category</div>
+          <div class="details-bar">
+            <div class="details-bar-bg" style="width= 250"></div>
+          </div>
+        </div><div class="details-row">
+          <div>Product_type</div>
+          <div class="details-bar">
+            <div class="details-bar-bg" style="width= 250">bronzer</div>
+          </div>
+        </div></section>`;
 }
 
 // function load(products) {
